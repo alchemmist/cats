fe8309281270c648d0abf2c52d9879cf8938ed2b
Mark Meliksetyan <markmelix@gmail.com>
2025-04-24 20:19:34 +0300

    impl header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,20 @@
 import "./Header.css";
 import { Link } from "react-router";
+import * as auth from "../../stores/auth";
 
 function Header() {
   return (
     <nav>
-      <Link to="/">Главное</Link>
-      <Link to="/favourites">Избранное</Link>
-      <Link to="/me">Профиль</Link>
+      <div className="main-side">
+        <Link to="/">Главная</Link>
+        <Link to="/favorites">Избранное</Link>
+      </div>
+      <div className="profile-side">
+        <Link to="/profile">
+          <img alt="Аватарка" src="/src/assets/user.png" />
+          {auth.isAuthorized() && "Профиль"}
+        </Link>
+      </div>
     </nav>
   );
 }
